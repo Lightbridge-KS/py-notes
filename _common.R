f26edef2092d00527a458ea8c61298f165dd38cd
Lightbridge-KS <ki11ip0.s.a.s@gmail.com>
2024-02-17 17:17:46 +0700

    update

--- _common.R
+++ _common.R
@@ -24,21 +24,21 @@ options(
 
 
 # Make error messages closer to base R
-registerS3method("wrap", "error", envir = asNamespace("knitr"),
-                 function(x, options) {
-                   msg <- conditionMessage(x)
-                   
-                   call <- conditionCall(x)
-                   if (is.null(call)) {
-                     msg <- paste0("Error: ", msg)
-                   } else {
-                     msg <- paste0("Error in ", deparse(call)[[1]], ": ", msg)
-                   }
-                   
-                   msg <- error_wrap(msg)
-                   knitr:::msg_wrap(msg, "error", options)
-                 }
-)
+# registerS3method("wrap", "error", envir = asNamespace("knitr"),
+#                  function(x, options) {
+#                    msg <- conditionMessage(x)
+#                    
+#                    call <- conditionCall(x)
+#                    if (is.null(call)) {
+#                      msg <- paste0("Error: ", msg)
+#                    } else {
+#                      msg <- paste0("Error in ", deparse(call)[[1]], ": ", msg)
+#                    }
+#                    
+#                    msg <- error_wrap(msg)
+#                    knitr:::msg_wrap(msg, "error", options)
+#                  }
+# )
 
 error_wrap <- function(x, width = getOption("width")) {
   lines <- strsplit(x, "\n", fixed = TRUE)[[1]]
